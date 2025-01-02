44aa018edb2017e33f93b1fb6c6e9020034221f4
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-01-02 11:21:23 -0500

    Finished with the remove button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const bookTable = document.querySelector('#book-table tbody');
 const formDialog = document.querySelector('#book-form-dialog');
 const newButton = document.querySelector('#new-button');
+const sumbitButton = document.querySelector('#sumbit');
+const bookContainer = document.querySelector('.book-container');
 
 const myLibrary = [];
 
@@ -18,43 +19,56 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 };
 
-addBookToLibrary("Test", "Test", 12, true);
-addBookToLibrary("Test", "Test", 12, true);
-addBookToLibrary("Test", "Test", 12, false);
-addBookToLibrary("Test", "Test", 12, false);
+function display() {
+    let bookCard = "";
+    for (const book of myLibrary) {
+        bookCard += `<div class="book-card book-idx-${myLibrary.indexOf(book)}">
+                <h2 class="book-title">${book.title}</h2>
+                <p class="book-author">${book.author}</p>
+                <p class="book-pages">${book.pages} pages</p>
+                <div class="read-status">
+                    <label for="toggle" class="read-label">Read Status</label>
+                    <label class="toggle-switch">
+                        <input type="checkbox" id="toggle" ${book.read ? "checked" : ""}>
+                        <span class="slider"></span>
+                    </label>
+                </div>
+                <div class="card-buttons">
+                    <button class="edit-button">Edit</button>
+                    <button class="remove-button">Remove</button>
+                </div>
+            </div>`;
+    }
 
-
-function display(){
-    myLibrary.forEach( book => {
-        const newRow = document.createElement("tr");
-        const titleCell = document.createElement("td");
-        const authorCell = document.createElement("td");
-        const pagesCell = document.createElement("td");
-        const readCell = document.createElement("td");
-    
-        titleCell.textContent = book.title;
-        authorCell.textContent = book.author;
-        pagesCell.textContent = book.pages;
-        readCell.textContent = book.read;
-    
-        newRow.appendChild(titleCell);
-        newRow.appendChild(authorCell);
-        newRow.appendChild(pagesCell);
-        newRow.appendChild(readCell); 
-
-        bookTable.appendChild(newRow);
-    });
+    bookContainer.innerHTML = bookCard;
+    deleteBook();
 };
+
+function deleteBook() {
+    let removeButtons = document.querySelectorAll('.remove-button');
+    removeButtons.forEach((removeButton) => {
+        removeButton.addEventListener("click", function () {
+            let word = removeButton.parentElement.parentElement.className.split(" ")[1].split("-")[2];
+            removeButton.parentElement.parentElement.remove();
+            myLibrary.splice(word, 1);
+            console.log("Library elements: " + myLibrary.length);     
+        });
+    });
+}
 
 newButton.addEventListener("click", function () {
     formDialog.showModal();
 });
 
-formDialog.addEventListener("click", function(event) {
-    if (event.target === formDialog) { 
-        formDialog.close(); 
-    }
+sumbitButton.addEventListener("click", function () {
+    event.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked;
+
+    formDialog.close();
+    addBookToLibrary(title, author, pages, read);
+    display();
 });
-
-
-display();
